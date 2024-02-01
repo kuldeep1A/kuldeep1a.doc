2c7e73440f44670c6f9810510374249938f7cd6d
KULDEEP DHANGAR <90776144+kuldeep1A@users.noreply.github.com>
2024-02-01 13:26:08 +0530

    chagne doc

--- src/pages/p-1-docs.tsx
+++ src/pages/p-1-docs.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function p1() {
     return (
         <div className="main-docs">
-            <iframe width="100%" height="100%" title="p-1-docs" src="https://docs.google.com/document/d/e/2PACX-1vTifrEuSp4QgRXK35Ud17eoqlzsvqYpimBepYmuHuBruz7ZbT9SyTxNILeQYg4vYJ4RWKQtLDFgB1ul/pub?embedded=true" />
+            <iframe width="100%" height="100%" title="p-1-docs" src="abc" />
         </div>
     );
 }
